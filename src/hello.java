009fed3e6352573403100646deeaa534c5e247f9
chenjinpeng <1559827725@qq.com>
2021-09-12 17:44:15 +0800

    提交src

---
+++ src/hello.java
@@ -0,0 +1,15 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好呀后哈哈哈哈");
+        System.out.println("你好，git");
+    }
+}
